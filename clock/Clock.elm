1061ea6daea0236cb1771f5a7bfda6dd8ff01fe5
danigb <danigb@gmail.com>
2016-05-13 12:07:14 +0200

    More exercices

--- clock/Clock.elm
+++ clock/Clock.elm
@@ -3,6 +3,7 @@ import Html.App exposing (program)
 import Svg exposing (circle, line, svg)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
+import Date
 
 
 main =
@@ -16,21 +17,28 @@ type alias Model = Time
 
 -- VIEW
 
+hand color angle =
+  let
+    handX = toString (50 + 40 * cos angle)
+    handY = toString (50 + 40 * sin angle)
+  in
+    line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke color] []
+
+-- https://en.wikipedia.org/wiki/Clock_angle_problem
 view : Model -> Html Msg
 view model =
   let
-    angle =
-      turns (Time.inMinutes model)
-
-    handX =
-      toString (50 + 40 * cos angle)
-
-    handY =
-      toString (50 + 40 * sin angle)
+    date = Date.fromTime model
+    sAng = degrees (toFloat (6 * Date.second date))
+    mAng = degrees (6 * Time.inMinutes model)
+    hAng = degrees (6 * Time.inMinutes model)
+    h = Debug.log "hours" (Time.inHours model)
   in
     svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+      [ circle [ cx "50", cy "50", r "45", fill "#EFEFEF" ] []
+      , hand "red" sAng
+      , hand "orange" mAng
+      , hand "blue" hAng
       ]
 
 
