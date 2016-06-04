2c25918fc4e62e60cb93e116b83e899384fdf0f8
danigb <danigb@gmail.com>
2016-06-04 10:13:20 +0200

    Randoms, part 1

--- music1/src/Music.elm
+++ music1/src/Music.elm
@@ -3,25 +3,50 @@ import Html exposing (Html, div, text)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (href)
 import Html.App as App
+
+import Time
+import Task
+import Random
+
 import Ports exposing (play, Event)
 
 
-chromatic =
-  List.indexedMap (\i n -> Event (0.2 * toFloat i) n) (List.append [60..72] (List.reverse [60..71]))
+phrase : Float -> List Int -> List Event
+phrase dur notes =
+  List.indexedMap (\i n -> Event (dur * toFloat i) n) notes
 
+chromatic : List Int
+chromatic =
+  List.append [60..72] (List.reverse [60..71])
+
+
+
+randomList n =
+  Random.list n (Random.int 40 90)
 
 
 main =
   App.program { init = init, view = view, update = update, subscriptions = subscriptions }
 
-type alias Model = { ready : Bool, inst : String }
+type alias Model = { ready : Bool, seed : Random.Seed, inst : String }
 
 type Msg =
-  Loaded String
-  | Example1
+  NewSeed Random.Seed
+  | Loaded String
+  | ChromaticExample
+  | RandomExample
+  | RandomList (List Int)
+
+
+getCurrentSecs : Cmd Msg
+getCurrentSecs =
+  let
+    timeToSeed t = Random.initialSeed (floor (Time.inSeconds t))
+  in
+    Task.perform (\_ -> NewSeed (timeToSeed 0)) (\t -> NewSeed (timeToSeed t)) Time.now
 
 init : (Model, Cmd Msg)
-init = ({ ready = False, inst = "" }, Cmd.none)
+init = ({ ready = False, inst = "", seed = Random.initialSeed 0 }, getCurrentSecs)
 
 view : Model -> Html Msg
 view model =
@@ -30,7 +55,8 @@ view model =
     viewLoading = div [] [text "Loading piano..."]
     viewLoaded name = div [] [
       Html.h2 [] [text ("Instrument: " ++ name)],
-      Html.a [href "#", onClick Example1] [text "chromatic"]
+      Html.a [href "#", onClick ChromaticExample] [text "chromatic"], Html.br [] [],
+      Html.a [href "#", onClick RandomExample] [text "random"]
     ]
   in
     Html.div [] [viewHead, if model.ready then viewLoaded model.inst else viewLoading]
@@ -39,8 +65,11 @@ view model =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Example1 -> (model, Ports.schedule chromatic)
-    Loaded name -> ({ ready = True, inst = name }, play "a3")
+    NewSeed s -> Debug.log "seed" ({ model | seed = s }, Cmd.none)
+    Loaded name -> ({ model | ready = True, inst = name }, play "a3")
+    ChromaticExample -> (model, Ports.schedule (phrase 0.2 chromatic))
+    RandomExample -> (model, Random.generate RandomList (randomList 10))
+    RandomList list -> (model, Ports.schedule (phrase 0.5 list))
 
 
 subscriptions : Model -> Sub Msg
